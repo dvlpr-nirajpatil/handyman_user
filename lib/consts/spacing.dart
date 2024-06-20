2471773a8c823c0d414defa18bd039e152c54b9d
Aditya Aher <adityaaher@Adityas-MacBook-Air.local>
2024-06-20 14:40:32 +0530

    height box added

--- lib/consts/spacing.dart
+++ lib/consts/spacing.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Spacing {
   static EdgeInsets screenPadding = EdgeInsets.symmetric(horizontal: 20);
+
+  static SizedBox heightBox(double value) {
+    return SizedBox(
+      height: value.h,
+    );
+  }
 }
